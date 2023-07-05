c6b4852a06cd4da43c75876b2ea20a627e03c4ac
Pedro Henrique <cezaogamer@gmail.com>
2023-07-05 11:39:55 -0300

    alteracao.js

--- js/favorites.js
+++ js/favorites.js
@@ -3,6 +3,26 @@
 class favorites{
     constructor(root){
         this.root = document.querySelector('root')
+        
+        this.load()
+    }
+
+    load(){
+        this.entries = [
+            {
+                login:'cthukey',
+                name:'Pedro Henrique',
+                public_repos:'76',
+                followers:'120000',
+            },
+
+            {
+                login:'diego3g',
+                name:'Diego fernandes',
+                public_repos:'78',
+                followers:'120000',
+            }
+        ]
     }
 }
 
@@ -14,17 +34,66 @@ class favorites{
 export class favoriteView extends favorites{
     constructor(root){
         super(root)
+        
+        this.tbody = document.querySelector('table tbody')
 
         this.update()
     }
 
     update(){
         this.removeAlltr()
+
+        this.entries.forEach(user => {
+            const row = this.createRow()
+            // console.log(row)
+            
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user p').textContent = `${user.name}`
+            row.querySelector('.user span').textContent = `${user.login}`
+            row.querySelector('.repositories').textContent = `${user.public_repos}`
+            row.querySelector('.followers').textContent = `${user.followers}`
+
+
+
+
+
+
+
+
+
+            this.tbody.append(row)//(append)Ele vai receber a @row para q seja Html
+        })
+    }
+
+    createRow(){
+        const tr = document.createElement('tr')
+
+        tr.innerHTML = `
+        
+        <td class="user">
+            <img src="https://github.com/cthukey.png" alt="Image de perfil">
+            <a href="https://github.com/cthukey" target="_blank">
+                <p>Pedro Henrique</p>
+                <span>/cthukey</span>
+            </a>
+            </td>
+            <td class="repositories">
+                123
+            </td>
+            <td class="followers">
+                1234
+            </td>
+            <td>
+                <button class="remove">Remover</button>
+            </td>
+
+        `
+
+        return tr
     }
 
     removeAlltr(){
-        const tbody = document.querySelector('table tbody')
-        tbody.querySelectorAll('tr').
+        this.tbody.querySelectorAll('tr').
         forEach((tr)=>{
             tr.remove()
         })
